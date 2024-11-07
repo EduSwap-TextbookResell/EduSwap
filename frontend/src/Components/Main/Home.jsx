598e24395812312083d11f11c4113841948afe9a
Julia <102248096+JustJuulia@users.noreply.github.com>
2024-11-07 23:52:06 +0100

    added main style
    i gotta make it all scaleable tho

--- frontend/src/Components/Main/Home.jsx
+++ frontend/src/Components/Main/Home.jsx
@@ -1,25 +1,39 @@
 //import { useState } from "react";
-import Navbar from "./Navbar.jsx";
-import { useLocation } from "react-router-dom";
+import Navbar from './Navbar.jsx';
+import { useLocation } from 'react-router-dom';
 import background_img from '../../assets/Main/background.png';
+import search_icon from '../../assets/Main/search.png';
+import messages_icon from '../../assets/Main/messages.png';
+import addbook_icon from '../../assets/Main/addbook.png'
 export default function Home() {
-
-	const location = useLocation();
-	const user = location.state?.user;
-	return (
-		<div className="flex min-h-screen justify-center bg-light_background bg-blend-darken"
-		style={{ backgroundImage: `url(${background_img})` }}>
-		<div className="flex max-w-screen ">
-			<div>
-			<div>
-			<Navbar user={user} />
-			</div>
-			<div className="relative">
-					<div className="bg-light_very_gray rounded-[45px] w-[15vw] h-[14vh] absolute left-0 top-[-8%] ml-64 border-4 border-black"> asdadas</div>
-                    <div className="center bg-light_very_gray rounded-[69px] m-auto w-[70vw] h-[75vh] mt-16"> </div>
+  const location = useLocation();
+  const user = location.state?.user;
+  return (
+    <div
+      className="flex min-h-screen justify-center bg-light_background bg-blend-darken"
+      style={{ backgroundImage: `url(${background_img})` }}
+    >
+      <div className="max-w-screen flex">
+        <div>
+          <div>
+            <Navbar user={user} className='drop-shadow-2xl' />
+          </div>
+          <div className="mt-8 flex flex-col items-center gap-8">
+            <div className="flex mr-36">
+              <div className=" h-[20vh] w-[18vw] rounded-[20px] bg-light_very_gray drop-shadow-xl">
+                <div className='flex'><img src={search_icon} className='ml-5 mt-1'/><p className="mt-2 ml-6 text-center text-light_coral ">Wyszukiwarka</p></div>
+              </div>
+              <div className="h-[20vh] w-[18vw] rounded-[20px] bg-light_gray ml-6 drop-shadow-xl">
+			  <div className='flex'><img src={addbook_icon} className='ml-5 mt-2'/><p className="mt-2 ml-6 text-center text-light_coral ">Wystaw książkę</p></div>
+              </div>
+              <div className="h-[20vh] w-[18vw] rounded-[20px] bg-light_gray ml-6 drop-shadow-xl">
+			  <div className='flex'><img src={messages_icon} className='ml-5 mt-2'/><p className="mt-2 ml-6 text-center text-light_coral ">Wiadomości</p></div>
+              </div>
             </div>
-			</div>
-		</div>
-	</div>
-	);
+            <div className="mt-[-9%] h-[80vh] w-[80vw] rounded-[69px] bg-light_very_gray drop-shadow-xl"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/Components/Main/Navbar.jsx
+++ frontend/src/Components/Main/Navbar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import logo_icon from '../../assets/Main/logo2.png';
+import profile_icon from '../../assets/Main/profile.png'
 export default function Navbar(props) {
 	const [menu, setMenu] = useState(false);
 	const location = useLocation();
@@ -12,16 +14,20 @@ export default function Navbar(props) {
 					<img src="../../assets/Login_Registration/hand_money.png" alt="" />
 				</div> */}
 				<div>
+					<img src={logo_icon} className="h-12"/>
 					{props.user ? (
-						<div onClick={()=>setMenu(!menu)} className="select-none">Hello {user.nick}</div>
+						<div onClick={()=>setMenu(!menu)} className="select-none ml-[84rem] mt-[-2.5%] flex">
+							<div className="ml-[-44%] mb-6"><img src={profile_icon} className="h-9 w-9 rounded-full mt-auto" /></div>
+							<p className="pl-8 mt-1 text-light_coral">Hello {user.nick}</p>
+							</div>
 					) : (
-						<div>
+						<div className=" ml-[84rem] mt-[-2.5%]">
 							<Link to="/signup/login">Login</Link>
 							<Link to="/signup/register">Register</Link>
 						</div>
 					)}
 					{menu ? (
-						<div className="flex flex-col">
+						<div className="flex flex-col ml-[84rem] mt-3 bg-light_gray bg-blend-darken">
 							<Link to="/dashboard">Dashboard</Link>
 							<Link to="/profile">Profile</Link>
 							<Link to="/logout">Logout</Link>
