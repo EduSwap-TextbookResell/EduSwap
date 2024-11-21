ffb3b2189b88e5340ed37802c25d907ac4a8adeb
sikoramodra <wojciechmodro@gmail.com>
2024-11-21 15:07:05 +0100

    fix client port customization

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,24 @@
 // eslint-disable-next-line import/namespace
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  preview: {
-    port: 8080,
-    strictPort: true,
-  },
-  server: {
-    port: 8080,
-    strictPort: true,
-    host: true,
-    historyApiFallback: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()); 
+  const PORT = `${env.VITE_CLIENT_PORT ?? '8080'}`;
+
+  return {
+    base: '/',
+    plugins: [react()],
+    preview: {
+      port: PORT,
+      strictPort: true,
+    },
+    server: {
+      port: PORT,
+      strictPort: true,
+      host: true,
+      historyApiFallback: true,
+    },
+  }
 });
