64363989c14afd518c22195dfc683f6be251a609
sikoramodra <wojciechmodro@gmail.com>
2025-03-12 19:06:51 +0100

    update lockfiles

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,33 +1,36 @@
-import { useState } from "react";
-import Navbar from "./Navbar.jsx";
-import Background_Img from "../../assets/Main/background.png";
+import { useState } from 'react';
+import Navbar from './Navbar.jsx';
+import Background_Img from '../../assets/Main/background.png';
 import Main from './Main/Main.jsx';
 
 export default function Home() {
-	const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-	const getUser = () => {
-		// fetch user data
-		setUser({ username: "admin" });
-	};
+  const getUser = () => {
+    // fetch user data
+    setUser({ username: 'admin' });
+  };
 
-	const logout = () => {
-		// logout user
-		setUser(null);
-	}
+  const logout = () => {
+    // logout user
+    setUser(null);
+  };
 
-	return (
-		<div className="bg-light_background bg-blend-darken h-full" style={{ backgroundImage: `url(${Background_Img})` }}>
-			<Navbar user={user} logout={logout} />
-			<div className="flex items-center justify-center mt-4 right-2 absolute">
-				<input
-					type="button"
-					value="Add User"
-					onClick={getUser}
-					className="bg-dark_coral rounded-2xl p-2 font-bold border cursor-pointer"
-				/>
-			</div>
-			<Main />
-		</div>
-	);
+  return (
+    <div
+      className="h-full bg-light_background bg-blend-darken"
+      style={{ backgroundImage: `url(${Background_Img})` }}
+    >
+      <Navbar user={user} logout={logout} />
+      <div className="absolute right-2 mt-4 flex items-center justify-center">
+        <input
+          type="button"
+          value="Add User"
+          onClick={getUser}
+          className="cursor-pointer rounded-2xl border bg-dark_coral p-2 font-bold"
+        />
+      </div>
+      <Main />
+    </div>
+  );
 }
--- frontend/src/Components/SignUp/Forms/Registration_Form.jsx
+++ frontend/src/Components/SignUp/Forms/Registration_Form.jsx
@@ -1,161 +1,200 @@
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
-import { TextField, Typography, Button, InputLabel, MenuItem, FormControl, Select } from "@mui/material";
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import {
+  TextField,
+  Typography,
+  Button,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@mui/material';
 
 export default function RegistrationForm() {
-	const {
-		register,
-		handleSubmit,
-		setValue,
-		watch,
-		trigger,
-		formState: { errors },
-	} = useForm();
-	const city = watch("city", "");
-	const school = watch("school", "");
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    trigger,
+    formState: { errors },
+  } = useForm();
+  const city = watch('city', '');
+  const school = watch('school', '');
 
-	const onSubmit = (data) => {
-		// eslint-disable-next-line no-unused-vars
-		const { confirmPassword, ...rest } = data;
-		console.log(rest);
-	};
+  const onSubmit = (data) => {
+    // eslint-disable-next-line no-unused-vars
+    const { confirmPassword, ...rest } = data;
+    console.log(rest);
+  };
 
-	const change_city = (event) => {
-		const value = event.target.value;
-		setValue("city", value);
-		trigger("city");
-	};
+  const change_city = (event) => {
+    const value = event.target.value;
+    setValue('city', value);
+    trigger('city');
+  };
 
-	const change_school = (event) => {
-		const value = event.target.value;
-		setValue("school", value);
-		trigger("school");
-	};
+  const change_school = (event) => {
+    const value = event.target.value;
+    setValue('school', value);
+    trigger('school');
+  };
 
-	return (
-		<div className="flex flex-col items-center justify-center h-full p-8">
-			{/* Header */}
-			<div className="mb-8 text-2xl font-bold text-center">
-				Witamy w<span className="text-dark_coral"> E</span>du
-				<span className="text-dark_coral">S</span>wap!
-			</div>
+  return (
+    <div className="flex h-full flex-col items-center justify-center p-8">
+      {/* Header */}
+      <div className="mb-8 text-center text-2xl font-bold">
+        Witamy w<span className="text-dark_coral"> E</span>du
+        <span className="text-dark_coral">S</span>wap!
+      </div>
 
-			{/* Form */}
-			<form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md space-y-6">
-				{/* Email Field */}
-				<TextField
-					label="Mail"
-					error={!!errors.email}
-					variant="outlined"
-					fullWidth
-					size="small"
-					{...register("email", {
-						required: "Mail jest wymagany",
-						pattern: {
-							value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-							message: "Niepoprawny adres mailowy",
-						},
-					})}
-				/>
-				{errors.email && <Typography color="error">{errors.email.message}</Typography>}
+      {/* Form */}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-md space-y-6"
+      >
+        {/* Email Field */}
+        <TextField
+          label="Mail"
+          error={!!errors.email}
+          variant="outlined"
+          fullWidth
+          size="small"
+          {...register('email', {
+            required: 'Mail jest wymagany',
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: 'Niepoprawny adres mailowy',
+            },
+          })}
+        />
+        {errors.email && (
+          <Typography color="error">{errors.email.message}</Typography>
+        )}
 
-				{/* Nick Field */}
-				<TextField
-					label="Nick"
-					error={!!errors.nick}
-					variant="outlined"
-					fullWidth
-					size="small"
-					{...register("nick", {
-						required: "Nick jest wymagany",
-						pattern: {
-							value: /^[A-Za-z0-9_]+$/i,
-							message: "Nick może posiadać tylko literki, cyferki oraz podłogi",
-						},
-					})}
-				/>
-				{errors.nick && <Typography color="error">{errors.nick.message}</Typography>}
+        {/* Nick Field */}
+        <TextField
+          label="Nick"
+          error={!!errors.nick}
+          variant="outlined"
+          fullWidth
+          size="small"
+          {...register('nick', {
+            required: 'Nick jest wymagany',
+            pattern: {
+              value: /^[A-Za-z0-9_]+$/i,
+              message: 'Nick może posiadać tylko literki, cyferki oraz podłogi',
+            },
+          })}
+        />
+        {errors.nick && (
+          <Typography color="error">{errors.nick.message}</Typography>
+        )}
 
-				{/* City Selection */}
-				<FormControl fullWidth size="small">
-					<InputLabel id="city-label">Wybierz miasto</InputLabel>
-					<Select
-						labelId="city-label"
-						error={!!errors.city}
-						value={city}
-						label="Miasto"
-						onChange={change_city}
-						{...register("city", { required: "Wybranie miasta jest wymagane" })}
-					>
-						<MenuItem value="city1">Szczecin</MenuItem>
-						<MenuItem value="city2">Warszawa</MenuItem>
-						<MenuItem value="city3">Kraków</MenuItem>
-					</Select>
-				</FormControl>
-				{errors.city && <Typography color="error">{errors.city.message}</Typography>}
+        {/* City Selection */}
+        <FormControl fullWidth size="small">
+          <InputLabel id="city-label">Wybierz miasto</InputLabel>
+          <Select
+            labelId="city-label"
+            error={!!errors.city}
+            value={city}
+            label="Miasto"
+            onChange={change_city}
+            {...register('city', { required: 'Wybranie miasta jest wymagane' })}
+          >
+            <MenuItem value="city1">Szczecin</MenuItem>
+            <MenuItem value="city2">Warszawa</MenuItem>
+            <MenuItem value="city3">Kraków</MenuItem>
+          </Select>
+        </FormControl>
+        {errors.city && (
+          <Typography color="error">{errors.city.message}</Typography>
+        )}
 
-				{/* School Selection */}
-				<FormControl fullWidth size="small">
-					<InputLabel id="school-label">Wybierz szkołę</InputLabel>
-					<Select
-						labelId="school-label"
-						disabled={!city}
-						error={!!errors.school}
-						value={school}
-						label="Szkola"
-						onChange={change_school}
-						{...register("school", { required: "Wybranie szkoly jest wymagane" })}
-					>
-						<MenuItem value="szkola1">Szczecińskie Collegium Depresji</MenuItem>
-						<MenuItem value="szkola2">Technikum Może Elektryczne</MenuItem>
-						<MenuItem value="szkola3">Chmurka Zgfburka</MenuItem>
-					</Select>
-				</FormControl>
-				{errors.school && <Typography color="error">{errors.school.message}</Typography>}
+        {/* School Selection */}
+        <FormControl fullWidth size="small">
+          <InputLabel id="school-label">Wybierz szkołę</InputLabel>
+          <Select
+            labelId="school-label"
+            disabled={!city}
+            error={!!errors.school}
+            value={school}
+            label="Szkola"
+            onChange={change_school}
+            {...register('school', {
+              required: 'Wybranie szkoly jest wymagane',
+            })}
+          >
+            <MenuItem value="szkola1">Szczecińskie Collegium Depresji</MenuItem>
+            <MenuItem value="szkola2">Technikum Może Elektryczne</MenuItem>
+            <MenuItem value="szkola3">Chmurka Zgfburka</MenuItem>
+          </Select>
+        </FormControl>
+        {errors.school && (
+          <Typography color="error">{errors.school.message}</Typography>
+        )}
 
-				{/* Password Field */}
-				<TextField
-					label="Hasło"
-					error={!!errors.password}
-					variant="outlined"
-					type="password"
-					fullWidth
-					size="small"
-					{...register("password", {
-						required: "Hasło jest wymagane",
-						minLength: { value: 6, message: "Hasło musi posiadać min. 6 znaków" },
-					})}
-				/>
-				{errors.password && <Typography color="error">{errors.password.message}</Typography>}
+        {/* Password Field */}
+        <TextField
+          label="Hasło"
+          error={!!errors.password}
+          variant="outlined"
+          type="password"
+          fullWidth
+          size="small"
+          {...register('password', {
+            required: 'Hasło jest wymagane',
+            minLength: {
+              value: 6,
+              message: 'Hasło musi posiadać min. 6 znaków',
+            },
+          })}
+        />
+        {errors.password && (
+          <Typography color="error">{errors.password.message}</Typography>
+        )}
 
-				{/* Confirm Password Field */}
-				<TextField
-					label="Powtorz haslo"
-					error={!!errors.confirmPassword}
-					variant="outlined"
-					type="password"
-					fullWidth
-					size="small"
-					{...register("confirmPassword", {
-						required: "Powtórzenie hasła jest wymagane",
-						validate: (value) => value === watch("password") || "Hasła się nie zgadzają",
-					})}
-				/>
-				{errors.confirmPassword && <Typography color="error">{errors.confirmPassword.message}</Typography>}
+        {/* Confirm Password Field */}
+        <TextField
+          label="Powtorz haslo"
+          error={!!errors.confirmPassword}
+          variant="outlined"
+          type="password"
+          fullWidth
+          size="small"
+          {...register('confirmPassword', {
+            required: 'Powtórzenie hasła jest wymagane',
+            validate: (value) =>
+              value === watch('password') || 'Hasła się nie zgadzają',
+          })}
+        />
+        {errors.confirmPassword && (
+          <Typography color="error">
+            {errors.confirmPassword.message}
+          </Typography>
+        )}
 
-				{/* Login Link */}
-				<div className="text-center text-sm">
-					Masz już konto?{" "}
-					<Link to="/signup/login" className="text-dark_coral hover:underline hover:underline-offset-2 hover:decoration-light-coral">
-						Zaloguj się
-					</Link>
-				</div>
+        {/* Login Link */}
+        <div className="text-center text-sm">
+          Masz już konto?{' '}
+          <Link
+            to="/signup/login"
+            className="hover:decoration-light-coral text-dark_coral hover:underline hover:underline-offset-2"
+          >
+            Zaloguj się
+          </Link>
+        </div>
 
-				{/* Submit Button */}
-				<Button variant="contained" type="submit" fullWidth sx={{ backgroundColor: "#E85A4F", padding: "12px" }}>
-					Zarejestruj się
-				</Button>
-			</form>
-		</div>
-	);
+        {/* Submit Button */}
+        <Button
+          variant="contained"
+          type="submit"
+          fullWidth
+          sx={{ backgroundColor: '#E85A4F', padding: '12px' }}
+        >
+          Zarejestruj się
+        </Button>
+      </form>
+    </div>
+  );
 }
